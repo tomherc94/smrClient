14e5e417d67b3652ccbce96e79b9fa2c695d2f12
Tomas Herculano <tomasherculano@gmail.com>
2020-04-26 20:12:33 -0300

    Envia o log para o Server via Socket com threads

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -1,65 +1,11 @@
 package aplication;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
-import org.hyperic.sigar.shell.ShellCommandExecException;
-import org.hyperic.sigar.shell.ShellCommandUsageException;
-
 public class Program {
 
-	public static void main(String[] args) throws ShellCommandUsageException, ShellCommandExecException{		
-		
-		SysInfo sys = new SysInfo();
-		sys.processCommand(args);
+	public static void main(String[] args) {
 
-		try {
-		
-		Socket cliente = new Socket(InetAddress.getLocalHost().getHostAddress(), 12345);
-        System.out.println("O cliente se conectou ao servidor!");
-
-        File f = new File("logs\\logCurrent.txt");
-		FileInputStream in = new FileInputStream(f);
-		
-		OutputStream out = cliente.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(out);
-		BufferedWriter writer = new BufferedWriter(osw);
-		writer.write(f.getName() + "\n");
-		writer.flush();
-		int tamanho = 4096; // buffer de 4KB  
-	    byte[] buffer = new byte[tamanho];  
-	    int lidos = -1;  
-	    while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
-	        out.write(buffer, 0, lidos);  
-	    } 
-	    
-        
-        
-        /*Scanner teclado = new Scanner(System.in);
-        PrintStream saida = new PrintStream(cliente.getOutputStream());
-
-        while (teclado.hasNextLine()) {
-            saida.println(teclado.nextLine());
-        }
-
-        saida.close();
-        teclado.close();*/
-	    in.close();
-        cliente.close();
-        
-        
-		}catch(UnknownHostException e) {
-			System.out.println(e.getMessage());
-		}catch(IOException e) {
-			System.out.println(e.getMessage());
-		}
+		ThreadSendLog sendLog = new ThreadSendLog(args, 10000);
+		sendLog.start();
 
 	}
 
---
+++ src/main/java/aplication/ThreadSendLog.java
@@ -0,0 +1,77 @@
+package aplication;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+import org.hyperic.sigar.shell.ShellCommandExecException;
+import org.hyperic.sigar.shell.ShellCommandUsageException;
+
+public class ThreadSendLog extends Thread {
+
+	private String[] args;
+	private Integer timeout;
+	private Socket client;
+
+	public ThreadSendLog() {
+
+	}
+
+	public ThreadSendLog(String[] args, Integer timeout) {
+
+		this.args = args;
+		this.timeout = timeout;
+	}
+
+	@Override
+	public void run() {
+		// Socket client = null;
+		System.out.println("O cliente se conectou ao servidor!");
+
+		try {
+			client = new Socket(InetAddress.getLocalHost().getHostAddress(), 12345);
+			do {
+				SysInfo sys = new SysInfo();
+				sys.processCommand(args);
+
+				
+
+				File f = new File("logs\\logCurrent.txt");
+				FileInputStream in = new FileInputStream(f);
+				
+				
+				OutputStream out = client.getOutputStream();
+				OutputStreamWriter osw = new OutputStreamWriter(out);
+				BufferedWriter writer = new BufferedWriter(osw);
+				writer.write(f.getName() + "\n");
+				writer.flush();
+				int tamanho = 4096; // buffer de 4KB
+				byte[] buffer = new byte[tamanho];
+				int lidos = -1;
+				while ((lidos = in.read(buffer, 0, tamanho)) != -1) {
+					out.write(buffer, 0, lidos);
+				}
+				Thread.sleep(timeout);
+				in.close();
+				//out.close();
+				//client.close();
+			}while (client.isConnected());
+			this.client.close();
+		} catch (ShellCommandUsageException e) {
+			System.out.println(e.getMessage());
+		} catch (ShellCommandExecException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
