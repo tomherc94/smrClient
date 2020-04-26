519c40a5a328deb3e0434582808097f01dd7661f
Tomas Herculano <tomasherculano@gmail.com>
2020-04-26 15:44:20 -0300

    Gera arquivo logCurrent.txt e envia para o Server via Socket

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -1,11 +1,14 @@
 package aplication;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import org.hyperic.sigar.shell.ShellCommandExecException;
 import org.hyperic.sigar.shell.ShellCommandUsageException;
@@ -16,15 +19,30 @@ public class Program {
 		
 		SysInfo sys = new SysInfo();
 		sys.processCommand(args);
-		
-		
-		
+
 		try {
 		
 		Socket cliente = new Socket(InetAddress.getLocalHost().getHostAddress(), 12345);
         System.out.println("O cliente se conectou ao servidor!");
 
-        Scanner teclado = new Scanner(System.in);
+        File f = new File("logs\\logCurrent.txt");
+		FileInputStream in = new FileInputStream(f);
+		
+		OutputStream out = cliente.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(out);
+		BufferedWriter writer = new BufferedWriter(osw);
+		writer.write(f.getName() + "\n");
+		writer.flush();
+		int tamanho = 4096; // buffer de 4KB  
+	    byte[] buffer = new byte[tamanho];  
+	    int lidos = -1;  
+	    while ((lidos = in.read(buffer, 0, tamanho)) != -1) {  
+	        out.write(buffer, 0, lidos);  
+	    } 
+	    
+        
+        
+        /*Scanner teclado = new Scanner(System.in);
         PrintStream saida = new PrintStream(cliente.getOutputStream());
 
         while (teclado.hasNextLine()) {
@@ -32,7 +50,8 @@ public class Program {
         }
 
         saida.close();
-        teclado.close();
+        teclado.close();*/
+	    in.close();
         cliente.close();
         
         
--- src/main/java/aplication/SysInfo.java
+++ src/main/java/aplication/SysInfo.java
@@ -1,7 +1,9 @@
 package aplication;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 
 import org.hyperic.sigar.FileSystem;
@@ -15,6 +17,7 @@ import org.hyperic.sigar.cmd.SigarCommandBase;
 public class SysInfo extends SigarCommandBase {
 
 	public final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+	public final SimpleDateFormat sdf2 = new SimpleDateFormat("ddMMyyyy-HHmmss");
 
 	public SysInfo(Shell shell) {
 		super(shell);
@@ -28,32 +31,49 @@ public class SysInfo extends SigarCommandBase {
 
 		// Data e Hora
 		String dateHour = sdf.format(new Date());
-		System.out.println(dateHour);
+		//System.out.println(dateHour);
 
 		// CPU
 		org.hyperic.sigar.CpuInfo[] infos = this.sigar.getCpuInfoList();
 		org.hyperic.sigar.CpuInfo cpu = infos[0];
-		System.out.println("CPU: " + cpu.getMhz() + " Mhz");
+		//System.out.println("CPU: " + cpu.getMhz() + " Mhz");
 
 		// RAM
 		Mem mem = this.sigar.getMem();
-		System.out.println("RAM Livre: " + mem.getActualFree() + " bytes");
+		//System.out.println("RAM Livre: " + mem.getActualFree() + " bytes");
 
 		// SWAP
 		Swap swap = this.sigar.getSwap();
-		System.out.println("SWAP Livre: " + swap.getFree() + " bytes");
+		//System.out.println("SWAP Livre: " + swap.getFree() + " bytes");
 
 		// Disk
-		System.out.println("File Systems........." + Arrays.asList(this.sigar.getFileSystemList()));
+		//System.out.println("File Systems........." + Arrays.asList(this.sigar.getFileSystemList()));
 		FileSystem[] fslist = this.sigar.getFileSystemList();
-		FileSystemUsage usage;
-		for(FileSystem f : fslist) {
+		FileSystemUsage usage = null;
+		for (FileSystem f : fslist) {
 			if (f.getType() == FileSystem.TYPE_LOCAL_DISK) {
 				usage = this.sigar.getFileSystemUsage(f.getDirName());
-				System.out.println("Disk used percent (" + f.getDirName() + "): " + usage.getUsePercent() * 100);		
+				//System.out.println("Disk used percent (" + f.getDirName() + "): " + usage.getUsePercent() * 100);
 			}
+		}
+		try {
+			FileWriter arq = new FileWriter("logs\\logCurrent.txt");
+			PrintWriter gravarArq = new PrintWriter(arq);
+
+			gravarArq.printf(dateHour);
+			gravarArq.print(";");
+			gravarArq.print(cpu.getMhz());
+			gravarArq.print(";");
+			gravarArq.print(mem.getActualFree());
+			gravarArq.print(";");
+			gravarArq.print(swap.getFree());
+			gravarArq.print(";");
+			gravarArq.print(usage.getUsePercent() * 100);
 			
+			arq.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
 }
